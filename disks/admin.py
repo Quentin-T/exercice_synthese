fa7fa85d4b32b58b3dc6be81102c619d025f328d
Quentin-T <quentin.thibaud@student.isae-supaero.fr>
2020-04-26 22:55:39 +0200

    ajout de l'administration, superuser : JohnDoe password: Sup3rUs3r

--- disks/admin.py
+++ disks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import album, artist, track
+
+admin.site.register(album)
+admin.site.register(artist)
+admin.site.register(track)
+
 
 # Register your models here.
